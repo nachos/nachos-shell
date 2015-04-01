729cd61f60d5dffe0671361df4d8a0b8d2bc04e9
Elad Bezalel <elad.bezalel@gmail.com>
2015-04-02 02:56:16 +0300

    movie-list caching and switching items

--- nachos-home/dips/movie-list/client/movie-list.js
+++ nachos-home/dips/movie-list/client/movie-list.js
@@ -5,32 +5,71 @@ angular.module('movieListApp')
     var movieList = require('movie-list');
     var movieInfo = require('movie-info');
     var _ = require('lodash');
+    var Q = require('q');
 
+    $scope.movies = [];
+    $scope.loading = true;
+
+    var sortMovies = function (movies){
+      return _.sortBy(movies, function (movie) {
+        return movie.response.imdbRating;
+      })
+        .reverse();
+    };
+
+    var getBackdrop = function (chosenMovie) {
+      var deferred = Q.defer();
+      if(!chosenMovie.backdrop) {
+        $scope.loading = true;
+        movieInfo(chosenMovie.title, function (err, res) {
+          if (err) {
+            deferred.reject(err);
+          } else {
+            chosenMovie.backdrop = "https://image.tmdb.org/t/p/w780" + res.backdrop_path;
+            deferred.resolve();
+          }
+        });
+      }
+      else{
+        deferred.resolve();
+      }
+      return deferred.promise;
+    };
+
+    $scope.chooseMovie = function(chosenMovie){
+      var movieIndex =_.findIndex($scope.movies, function (movie) {
+        return movie === chosenMovie;
+      });
+
+      $scope.movies.splice(movieIndex, 1);
+
+      if($scope.chosenMovie){
+        $scope.movies.push($scope.chosenMovie);
+      }
+
+      $scope.movies = sortMovies($scope.movies);
+      $scope.$apply();
+
+      getBackdrop(chosenMovie)
+          .then(function () {
+            $scope.chosenMovie = chosenMovie;
+            $scope.loading = false;
+            $scope.$apply();
+          });
+
+    };
 
     movieList.listFolder('E:\\Movies', function (err, listData) {
       if (err) {
         return console.log(err);
       }
 
-      console.log(listData);
+      $scope.movies = sortMovies(listData.succeeded);
 
-      var movies =
-        _.sortBy(listData.succeeded, function (movie) {
-          return movie.response.imdbRating;
-        })
-          .reverse();
 
-      var mostRanked = _.first(movies);
+      var mostRanked = _.first($scope.movies);
 
-      movieInfo(mostRanked.title, function (err, res) {
-        mostRanked.backdrop = "https://image.tmdb.org/t/p/w780" + res.backdrop_path;
+      $scope.chooseMovie(mostRanked);
 
-        $scope.mostRanked = mostRanked;
-        $scope.doneLoading = true;
-
-        $scope.$apply();
-      });
-
-      $scope.movies = _.drop(movies, 1);
     });
   });
