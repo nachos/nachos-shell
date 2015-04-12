7702b9a37631882d6f0490d06287d458c4711660
Elad Bezalel <elad.bezalel@gmail.com>
2015-04-13 01:16:54 +0300

    Few movie-list changes

--- nachos-home/dips/movie-list/client/movie-list.js
+++ nachos-home/dips/movie-list/client/movie-list.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('movieListApp')
-  .controller('MovieListController', function ($scope) {
+  .controller('MovieListController', function ($scope, $timeout) {
     var movieList = require('movie-list');
     var movieInfo = require('movie-info');
     var _ = require('lodash');
@@ -10,18 +10,18 @@ angular.module('movieListApp')
     $scope.movies = [];
     $scope.loading = true;
 
-    var sortMovies = function (movies){
+    var sortMovies = function (movies) {
       return _.sortBy(movies, function (movie) {
-        return movie.response.imdbRating;
+        return movie.imdbRating;
       })
         .reverse();
     };
 
     var getBackdrop = function (chosenMovie) {
       var deferred = Q.defer();
-      if(!chosenMovie.backdrop) {
+      if (!chosenMovie.backdrop) {
         $scope.loading = true;
-        movieInfo(chosenMovie.title, function (err, res) {
+        movieInfo(chosenMovie.Title, function (err, res) {
           if (err) {
             deferred.reject(err);
           } else {
@@ -30,42 +30,50 @@ angular.module('movieListApp')
           }
         });
       }
-      else{
+      else {
         deferred.resolve();
       }
       return deferred.promise;
     };
 
-    $scope.chooseMovie = function(chosenMovie){
-      var movieIndex =_.findIndex($scope.movies, function (movie) {
-        return movie === chosenMovie;
+    $scope.chooseMovie = function (chosenMovie) {
+      console.log(chosenMovie);
+      $timeout(function () {
+
+        var movieIndex = _.findIndex($scope.movies, function (movie) {
+          return movie === chosenMovie;
+        });
+
+        $scope.movies.splice(movieIndex, 1);
+
+        if ($scope.chosenMovie) {
+          $scope.movies.push($scope.chosenMovie);
+        }
+
+        $scope.movies = sortMovies($scope.movies);
       });
 
-      $scope.movies.splice(movieIndex, 1);
-
-      if($scope.chosenMovie){
-        $scope.movies.push($scope.chosenMovie);
-      }
-
-      $scope.movies = sortMovies($scope.movies);
-      $scope.$apply();
-
       getBackdrop(chosenMovie)
-          .then(function () {
+        .then(function () {
+          $timeout(function () {
             $scope.chosenMovie = chosenMovie;
             $scope.loading = false;
-            $scope.$apply();
           });
-
+        });
     };
 
-    movieList.listFolder('D:\\Videos\\Movies', function (err, listData) {
+    movieList.listFolder('E:\\Movies', function (err, listData) {
       if (err) {
         return console.log(err);
       }
 
-      $scope.movies = sortMovies(listData.succeeded);
+      listData =_.map(listData.succeeded, function (item){
+        return item.response;
+      });
 
+      console.log(listData);
+
+      $scope.movies = sortMovies(listData);
 
       var mostRanked = _.first($scope.movies);
 
