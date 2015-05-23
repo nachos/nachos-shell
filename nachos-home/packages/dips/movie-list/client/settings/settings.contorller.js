a93dd77f2bf6b13cd71e71125c3b8d2347df9dd5
Elad Bezalel <elad.bezalel@gmail.com>
2015-05-23 17:31:15 +0300

    Save movie list settings

--- nachos-home/packages/dips/movie-list/client/settings/settings.contorller.js
+++ nachos-home/packages/dips/movie-list/client/settings/settings.contorller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('movieListSettingsApp')
-  .controller('SettingsController', function ($scope, $timeout) {
+  .controller('SettingsController', function ($scope, $timeout, $mdToast) {
     nachosApi.getSettings(function(err, config){
       if(err){
-        throw new Error(err);
+        notify(err);
       }
 
       $timeout(function () {
@@ -16,5 +16,29 @@ angular.module('movieListSettingsApp')
       $timeout(function () {
         $scope.config.directory = ele.files[0].path;
       });
+    };
+
+    $scope.save = function () {
+      nachosApi.saveSettings($scope.config, function (err) {
+        if(err){
+          notify(err);
+        }
+
+        notify('saved');
+      });
+    };
+
+    function notify (msg) {
+      $mdToast.show(
+        $mdToast.simple()
+          .content(msg)
+          .position({
+            bottom: true,
+            top: false,
+            left: false,
+            right: true
+          })
+          .hideDelay(3000)
+      );
     }
   });
